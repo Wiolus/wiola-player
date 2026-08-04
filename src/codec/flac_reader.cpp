a12c04f08f9ae1c2bd69768a5ea7ccbbf6a934db
Roman Glaz <70313579+Vokerlee@users.noreply.github.com>
2026-08-04 21:43:32 +0300

    codec: move format-readers headers from include to src (#35)

--- src/codec/flac_reader.cpp
+++ src/codec/flac_reader.cpp
@@ -18,7 +18,7 @@
  * along with Wiola. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <codec/flac_reader.hpp>
+#include "flac_reader.hpp"
 
 #define DR_FLAC_IMPLEMENTATION
 #include <dr_flac.h>
